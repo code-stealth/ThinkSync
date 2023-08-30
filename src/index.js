7048cce9eb13c5a74c57f091714f70cc4d324a73
Chirag Agarwal <code-stealth>
2023-08-30 17:39:48 +0530

    MongoDB connected to my server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const app = express();
 
+
+const connectDatabase = require("./dbconnect");
+
+connectDatabase();
 //importing the routes
 const userRouter = require("./routes/userRoutes");
 const noteRouter = require("./routes/noteRoutes");
 
-const app = express();
-
 // using the routes 
 app.use("/users", userRouter);
 app.use("/note", noteRouter);
+
+app.get("/", (req, res)=>{
+    res.send("Hello");
+});
 
 app.listen(5000, ()=>{
     console.log("Server started on port no. 5000");
