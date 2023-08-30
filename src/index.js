7f84df807eaaf768e31647c84fb7a82fd686611c
Chirag Agarwal <code-stealth>
2023-08-30 16:20:49 +0530

    Initial setup of project is done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello World");
+const express = require("express");
+
+//importing the routes
+const userRouter = require("./routes/userRoutes");
+const noteRouter = require("./routes/noteRoutes");
+
+const app = express();
+
+// using the routes 
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+
+app.listen(5000, ()=>{
+    console.log("Server started on port no. 5000");
+});
